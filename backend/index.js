90188c4dfcd2d2d412ac9747809582ad1dce3d7a
unknown <aneeshshillong3@gmail.com>
2021-08-02 04:11:51 +0530

    Add proc file and changed package.json

--- backend/index.js
+++ backend/index.js
@@ -23,19 +23,19 @@ app.use('/api/notes',noteRoutes);
 
 //--------------------------deployment ----------------------
 
-// __dirname=path.resolve();
-// if(process.env.NODE_ENV === 'production'){    
-//     app.use(express.static(path.join(__dirname,'/frontend/user-auth/build')))
+__dirname=path.resolve();
+if(process.env.NODE_ENV === 'production'){    
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
 
-//     app.get('*',(req,res)=>{
-//        res.sendFile(path.resolve(__dirname,'frontend','user-auth','build','index.html')) 
-//     })
+    app.get('*',(req,res)=>{
+       res.sendFile(path.resolve(__dirname,'frontend','build','index.html')) 
+    })
 
-// }else{
-//     app.get("/", (req, res) => {
-//         res.send("API is running..")
-//     })
-// }
+}else{
+    app.get("/", (req, res) => {
+        res.send("API is running..")
+    })
+}
 
 // app.get('/api/notes',(req,res)=>{
 //     res.json(notes)
